24b2c84b8ded002603fa1f44b087de1fc3a3343a
SZF-JamesD <052277@edu.szf.at>
2025-02-10 08:32:18 +0100

    Exercise 02-01

---
+++ KMS_06_LE_02/KMS_06_LE_02_01/src/NameInput.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class NameInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your first name: ");
+        String firstName = sc.nextLine();
+
+        System.out.println("Enter your last name: ");
+        String lastName = sc.nextLine();
+
+        System.out.println("Your full name is: " + firstName + " " + lastName);
+
+        sc.close();
+    }
+}
