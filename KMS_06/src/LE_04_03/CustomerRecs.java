6d8ed18fa3dcbe8b92b3a1d987fda9dc5b591518
SZF-JamesD <052277@edu.szf.at>
2025-02-11 12:08:02 +0100

    4.3

--- KMS_06/src/LE_04_03/CustomerRecs.java
+++ KMS_06/src/LE_04_03/CustomerRecs.java
@@ -6,10 +6,46 @@ import java.util.Random;
 public class CustomerRecs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
         int n;
-        System.out.println("Enter the number of reviews you require: ");
-        if (sc.hasNextInt()) {
-            n = sc.nextInt();
+        int[] results;
+        int bad = 0, okay = 0, good = 0;
+
+        while (true) {
+            System.out.println("Enter the number of reviews you require: ");
+            if (sc.hasNextInt()) {
+                n = sc.nextInt();
+                results = new int[n];
+                break;
+            } else{
+                System.out.println("Error: Please enter a positive integer.");
+            }
         }
+        for (int i = 0; i < n; i++) {
+            results[i] = rand.nextInt(3)+1;
+        }
+
+        for (int i : results) {
+            switch (i) {
+                case 1:
+                    bad += 1;
+                    break;
+                case 2:
+                    okay += 1;
+                    break;
+                case 3:
+                    good += 1;
+            }
+        }
+        System.out.println("Results: ");
+        System.out.println("Total votes: " + n);
+        System.out.printf("Bad: %d - %.2f%%\n", bad, (bad / (double)n) * 100);
+        System.out.printf("Okay: %d - %.2f%%\n", okay, (okay / (double)n) * 100);
+        System.out.printf("Good: %d - %.2f%%\n", good, (good / (double)n) * 100);
+
+        double overallRating = ((bad) + (2 * okay) + (3 * good)) / (double) n;
+        System.out.printf("Overall Rating: %.2f / 3",overallRating);
+
+
     }
 }
